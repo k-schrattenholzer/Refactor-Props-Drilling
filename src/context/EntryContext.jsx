4b2f2c1381fc9d82fbbda2c4c534eb2b2ba6fef5
‘Katie <ktschr11@gmail.com>
2021-12-13 14:24:09 -0800

    entry context written

--- src/context/EntryContext.jsx
+++ src/context/EntryContext.jsx
@@ -1,7 +1,27 @@
+import { useContext, createContext, useState} from 'react';
+
 //Create the Context
+const EntryContext = createContext();
 
 //Create the EntryProviderComponent
+const EntryProvider = ({ children }) => {
+  const [entries, setEntries] = useState([]);
+
+  return (
+    <EntryContext.Provider value={{ entries, setEntries }}>{children}</EntryContext.Provider>
+  )
+}
 
 //Create the custom hook
+const useEntries = () => {
+  //use of useContext is what makes this a custom hook
+  //if we attempt to use this outside of the provider, context will be undefined
+  const context = useContext(EntryContext);
+  if (context === undefined) {
+    throw newError('No, no, no. You are trying to use useEntries outside of an EntryContext Provider')
+  }
+  return context
+}
 
 //Export the provider component and the custom hook
+export { EntryProvider, useEntries }
